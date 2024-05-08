5c381688061e409bdc30985cfb66022f0894a1ee
RenzoZav <102383387+RenzoZav@users.noreply.github.com>
2024-05-08 10:55:18 -0300

    Subiendo lo que vimos en clases
    this

---
+++ FlujoDeDatos/src/main/java/Readers/main.java
@@ -0,0 +1,19 @@
+package Readers;
+
+import java.io.PrintStream;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		Lectura leer = new Lectura();
+		ClassStrings repaso = new ClassStrings();
+		//repaso.repaso();
+		
+		ClassReader lectura = new ClassReader();
+		//System.out.println( lectura.leer() );
+		
+			
+	}
+
+}
