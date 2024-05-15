9ecffc43c3ff544c07d0b698161bbc71b33b58db
RenzoZav <renzozavaletaet32@gmail.com>
2024-05-15 12:37:30 -0300

    guardando
    this

--- files/src/main/java/files/Ficheros.java
+++ files/src/main/java/files/Ficheros.java
@@ -1,5 +1,35 @@
 package files;
 
-public class Ficheros {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
+public class Ficheros {
+	
+	private File archivo;
+	
+	
+	public Ficheros() 
+	{
+		String ruta = "";	//  "c:\\User\\"
+		String nombre = "mario";
+		String extencion = ".txt";
+		
+		archivo = new File(ruta.concat(nombre.concat(extencion)));
+	}
+	
+	
+	
+	public void createrFilePrintStream(File a) 
+	{
+		FileOutputStream fos = null;
+		
+		try {
+			fos = new FileOutputStream(a);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
+
--- files/src/main/java/files/Main.java
+++ files/src/main/java/files/Main.java
@@ -3,7 +3,7 @@ package files;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 	}
 
 }
