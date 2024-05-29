3f3b59c6a6dbfeed971b48161df347001d0e228f
RenzoZav <102383387+RenzoZav@users.noreply.github.com>
2024-05-29 10:37:53 -0300

    Subiendo cambios
    This

--- files/src/main/java/files/Main.java
+++ files/src/main/java/files/Main.java
@@ -3,7 +3,10 @@ package files;
 public class Main {
 
 	public static void main(String[] args) {
+Ficheros archivo = new Ficheros("","Mario",".txt");
 		
+		//archivo.createFilePrintStream( archivo.getArchivo() );
+		archivo.createFilePrinter( archivo.getArchivo() );
 	}
 
 }
